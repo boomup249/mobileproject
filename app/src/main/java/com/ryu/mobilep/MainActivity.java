938dcfa6316566252c6690ba25c2381097db1723
류승균 <boomup249@naver.com>
2022-11-29 19:39:02 +0900

    fuck

--- app/src/main/java/com/ryu/mobilep/MainActivity.java
+++ app/src/main/java/com/ryu/mobilep/MainActivity.java
@@ -3,8 +3,12 @@ package com.ryu.mobilep;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mototal, moavg, jatotal, jaavg, ostotal, osavg;
+    EditText momid, mofin, moapp, jamid, jafin, jaapp, osmid, osfin, osapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
